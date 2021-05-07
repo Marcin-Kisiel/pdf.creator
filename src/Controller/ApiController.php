512e1aeb8204944928b27e79b563150f49e5073e
adamstyperek <adamstyperek@gmail.com>
2021-05-07 10:44:54 +0200

    string endpoint

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use Knp\Snappy\Pdf;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiController extends AbstractController
+{
+
+    const ENCODING = 'utf-8';
+
+    /**
+     * @\Symfony\Component\Routing\Annotation\Route("/string", name="pdf-string-output", methods={"POST"})
+     * @param Request $request
+     * @param Pdf $pdf
+     * @return Response
+     */
+    public function stringAction(Request $request, Pdf $pdf): Response
+    {
+        $html = $request->request->get('html');
+        $pdfParams = ['encoding' => self::ENCODING];
+        $pdf->getOutput($html, $pdfParams);
+        return new Response($pdf);
+
+    }
+}
